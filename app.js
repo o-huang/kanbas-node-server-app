f436e9258bdaa8ba5dc0e187eaecdb820c5fa111
Owen Huang <ohuang2@gmail.com>
2023-11-04 23:29:53 -0400

    push backend to github

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
-import express from 'express'
-import Hello from "./hello.js"
-const app = express()
-Hello(app)
-app.listen(4000)
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import cors from "cors";
+import "dotenv/config";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
+
+app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
+AssignmentRoutes(app);
+Hello(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
