297eb02fcb1ad578c7fe6eae60c6839b3ad394f0
Owen Huang <ohuang2@gmail.com>
2023-11-21 23:00:41 -0500

    test1

--- app.js
+++ app.js
@@ -9,44 +9,65 @@ import AssignmentRoutes from "./assignments/routes.js";
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
 import session from "express-session";
-const CONNECTION_STRING =
-  process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
-mongoose
-  .connect(CONNECTION_STRING)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((error) => {
-    console.error("Error connecting to MongoDB:", error);
-  });
+// const CONNECTION_STRING =
+//   process.env.DB_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+
+// mongoose
+//   .connect(CONNECTION_STRING)
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((error) => {
+//     console.error("Error connecting to MongoDB:", error);
+//   });
+
+// const app = express();
+
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin:
+//       process.env.NODE_ENV === "production"
+//         ? process.env.FRONTEND_URL
+//         : process.env.FRONTEND_URL_LOCAL,
+//   })
+// );
+// const sessionOptions = {
+//   secret: "any string",
+//   resave: false,
+//   saveUninitialized: false,
+// };
+
+// if (process.env.NODE_ENV !== "development") {
+//   sessionOptions.proxy = true;
+//   sessionOptions.cookie = {
+//     sameSite: "none",
+//     secure: true,
+//   };
+// }
+// app.use(session(sessionOptions));
+
+// app.use(express.json());
+// UserRoutes(app);
+// ModuleRoutes(app);
+// CourseRoutes(app);
+// AssignmentRoutes(app);
+// Hello(app);
+// Lab5(app);
+// app.listen(process.env.PORT || 4000);
 
 const app = express();
 
 app.use(
   cors({
     credentials: true,
-    origin:
-      process.env.NODE_ENV === "production"
-        ? process.env.FRONTEND_URL
-        : process.env.FRONTEND_URL_LOCAL,
+    origin: process.env.NODE_ENV === "production"
+      ? process.env.FRONTEND_URL
+      : process.env.FRONTEND_URL_LOCAL,
   })
 );
-const sessionOptions = {
-  secret: "any string",
-  resave: false,
-  saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-  };
-}
-app.use(session(sessionOptions));
 
 app.use(express.json());
-UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
